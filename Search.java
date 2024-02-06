35d1d08dd17a375c0624c5f7b34d84b45b31bb78
Your Name <you@example.com>
2024-02-06 22:45:30 +0200

    added the file to a arraylist

--- Search.java
+++ Search.java
@@ -2,13 +2,21 @@ import java.io.*;
 import java.util.*;
 public class Search {
     public static void main(String[] args) {
-        LinkedHashMap<String,Integer> stock =new LinkedHashMap<>();
+        LinkedHashMap<String,Integer> splitStock =new LinkedHashMap<>();
+        ArrayList<String> combinedValues=new ArrayList<>();
+
         // Scanner scanner =new Scanner(System.in);
         // System.out.println("++Kindly select your category 1 or 2.\n1. Client\n2. Employee");
         // String filename=scanner.nextLine();
         try{
-            Scanner scanner=new Scanner(new File("Stock.txt"));
-            
+            Scanner scanner=new Scanner(new File("Stock.txt"));//opens and reads the file
+            while (scanner.hasNextLine()) {
+                combinedValues.add(scanner.nextLine());//adds all the lines to the arraylist
+                
+
+                
+            }
+
 
         }catch(FileNotFoundException e){
             System.out.println("Database not UPDATED!!");
