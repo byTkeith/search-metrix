f57f0dc8a89e9dd021c6930a8f5c798d5482b199
Your Name <you@example.com>
2024-02-06 23:01:34 +0200

    added a method for value manipulation

--- Search.java
+++ Search.java
@@ -1,6 +1,12 @@
 import java.io.*;
 import java.util.*;
 public class Search {
+
+    public static String Manipulate(String string){
+        String[] splitBySpace=string.split(" ");
+        String shopItem=splitBySpace[0];
+        return "hi";
+    } 
     public static void main(String[] args) {
         LinkedHashMap<String,Integer> splitStock =new LinkedHashMap<>();
         ArrayList<String> combinedValues=new ArrayList<>();
@@ -12,14 +18,16 @@ public class Search {
             Scanner scanner=new Scanner(new File("Stock.txt"));//opens and reads the file
             while (scanner.hasNextLine()) {
                 combinedValues.add(scanner.nextLine());//adds all the lines to the arraylist
-                
-
-                
             }
 
 
         }catch(FileNotFoundException e){
             System.out.println("Database not UPDATED!!");
+        }
+        System.out.println("++Kindly select your category 1 or 2.\n1. Client\n2. Employee");
+        if()
+        for(String Line: combinedValues){
+            String eachLine=Manipulate(Line);
         }
     }
     
